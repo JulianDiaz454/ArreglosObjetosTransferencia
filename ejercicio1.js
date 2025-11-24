7640c472ae106a0ae46361ac6790e8fcb9b07ab3
Julian Diaz <torresanibal336@gmail.com>
2025-11-24 13:32:14 -0500

    Se realiza la solución dle ejercicio 1

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,1 +1,16 @@
 // Ejercicio 1
+
+// Se crea el arreglo con nombres de aprendices
+let aprendices = ["Julián", "Maria", "Valeria", "Willian", "Vanesa"];
+
+// Lista sin modificaciones
+console.log(aprrendices);
+
+// Se agrega un nombre al final
+aprendices.push("Andrés");
+
+// Se elimina el primer nombre
+aprendices.shift();
+
+// Se muestra el arreglo resultante en la consola
+console.log(aprendices);

4180865d83c20700685d8a4b688cbaa6a518f70d
Julian Diaz <torresanibal336@gmail.com>
2025-11-24 13:56:53 -0500

    Se realiza el ejercicio 8

---
+++ ejercicio8.js
@@ -0,0 +1,12 @@
+// Ejercicio 8
+
+// Objeto llamado libro
+let libro = {
+    "titulo": "Pinocho",
+    "autor": "Guillermo del toro",
+    "paginas": 350,
+    "genero": "Miedo",
+}
+
+// Mensaje descriptivo
+console.log(`El libro ${libro.titulo} del autor ${libro.autor} pertenece al genero ${libro.genero}`);

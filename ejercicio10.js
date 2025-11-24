36f53b90afa818f1b1b01dc1a51ee21a43bf1fa5
Julian Diaz <torresanibal336@gmail.com>
2025-11-24 14:02:54 -0500

    Se realiza el ejercicio 10

---
+++ ejercicio10.js
@@ -0,0 +1,23 @@
+// Ejercicio 10
+
+// Se observan las situaciones y se decide con que se maneja
+
+// A. lista de colores favoritos - Arreglo
+let colores = ["amarillo", "azul", "rojo",];
+
+// B. Informacion de un estudiante - Objeto
+let estudiante = {
+    "nombre": "Ana",
+    "edad": 20,
+    "carrera": "Ingeniería",
+};
+
+// C. Precios de diferentes camisas - Arreglo
+let preciosCamisas = [25000, 30000, 15000, 40000];
+
+// D. Descripción de un portatil - Objeto
+let portatil = {
+    "marca": "Dell",
+    "almacenamiento": "520GB",
+    "memoria": "16GB",
+};

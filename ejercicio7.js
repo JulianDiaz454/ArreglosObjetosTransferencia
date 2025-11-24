57d8d3f288ce4648718ceff604c7c9cea346193c
Julian Diaz <torresanibal336@gmail.com>
2025-11-24 13:52:52 -0500

    Se realiza el ejercicio 7

---
+++ ejercicio7.js
@@ -0,0 +1,11 @@
+// Ejercicio 7
+
+// Arreglo con edad de personas
+let edades = [12, 4, 65, 18, 23];
+
+// Se recorre el arreglo con for..of 
+for (let edad of edades) {
+    if (edad >= 18) {
+        console.log(`Es mayor: ${edad}`);
+    }
+}
